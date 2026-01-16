33f2a24afdde3ac1047f4362896ad7e4a8b80adf
John <43506685+Coniferish@users.noreply.github.com>
2026-01-16 09:42:49 -0500

    init exercise 1

--- src/exercises/Exercise1.jsx
+++ src/exercises/Exercise1.jsx
@@ -1,5 +1,16 @@
+/**  
+ * GET endpoint: https://jsonplaceholder.typicode.com/todos?_limit=10
+ * Todo item endpoint: https://jsonplaceholder.typicode.com/todos/:id
+ * Expected body: { userId: number, id: number, title: string , completed: boolean}
+ *
+ *  Challenge 1: 
+ *  Using HTTP requests to the API endpoint, get the first 10 todos and 
+ *  render them as a list with checkboxes that when checked send a request
+ *  indicating the todo item was completed.
+ */
+
 export default function Exercise1() {
     return (
-        <h1>Exercise 1</h1>
+        <h1>Welcome to exercise 1</h1>
     )
 }
