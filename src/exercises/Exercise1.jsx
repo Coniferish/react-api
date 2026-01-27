674840baae15d59a8b695137783308009127e395
John <43506685+Coniferish@users.noreply.github.com>
2026-01-26 21:03:56 -0500

    chore: handle clicking the checkbox
    Make a PATCH request to update the todo item on the backend and flip the state of the item's completed state.
    
    Also, make sure to use a function when updating the todos state instead of using the value from when the function is created/the component is rendered (pass a function to onChange instead of executing handleClick with the item). You always want to use a function when your new state depends on the previous state (especially in async functions or event handlers where the closure can end up having stale data).
    
    If we directly called handleToggle with the item directly from the onChange event, the state of the item (its item.complete state) could end up outdated if a user quickly toggled the checkbox before the fetch request completes. Instead, we can have React manage multiple state changes for us. By using a function you ensure that React uses the latest state because React will call your function with the latest state when the update is applied, not the state from when it was queued. By passing the function, you're telling React to "wait until you're ready to compute the state and when you do, use the latest state".
    
    TLDR: Use the functional form for event handlers when your new state depends on the previous state (especially in async functions or event handlers where the closure may hold stale data).

--- src/exercises/Exercise1.jsx
+++ src/exercises/Exercise1.jsx
@@ -48,7 +48,22 @@ export default function Exercise1() {
     }, []);
 
     async function handleToggle(item) {
-
+        try {
+            const response = await fetch(BASE_URL+`/${todos.id}`, {
+                method:'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ completed: !item.completed})
+            })
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`)
+            }
+            setTodos(todos => todos.map(t => t.id === item.id ? {...t, completed: !t.completed} : t))
+            console.log(response.status)
+        } catch (error) {
+            setError(error)
+        }
     }
 
     if (isLoading) {
